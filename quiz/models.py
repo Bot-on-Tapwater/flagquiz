779282139b952fbfd67201dbe8c2aaff76c7e6c6
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-07-15 18:10:04 +0300

    chore: Add record created at to userscore model

--- quiz/models.py
+++ quiz/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.timezone import now
+from datetime import timedelta
+from django.utils.timesince import timesince
 
 class Country(models.Model):
     name = models.CharField(max_length=100)
@@ -16,12 +19,15 @@ class Country(models.Model):
 class UserScore(models.Model):
     username = models.CharField(max_length=100)
     score = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.username}: {self.score}"
 
     def to_dict(self):
+            time_ago= timesince(self.created_at, now())
             return {
                 'username': self.username,
                 'score': self.score,
+                'created_at': f"{time_ago} ago"
             }
